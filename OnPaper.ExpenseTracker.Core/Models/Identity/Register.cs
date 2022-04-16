f1ff074e619fc341397eeeb1139859dfd2150d2a
Ceejay Pimentel <ceejaypimentel@hotmail.ca>
2022-04-16 18:30:36 -0500

    Login and Register

--- OnPaper.ExpenseTracker.Core/Models/Identity/Register.cs
+++ OnPaper.ExpenseTracker.Core/Models/Identity/Register.cs
@@ -1,7 +1,8 @@
 namespace OnPaper.ExpenseTracker.Core.Models.Identity;
 
-public abstract class Login
+public abstract class Register
 {
+    public string DisplayName { get; set; } = string.Empty;
     public string Email { get; set; } = string.Empty;
     public string Password { get; set; } = string.Empty;
 }
